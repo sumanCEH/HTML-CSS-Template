5f21cc8c23da77ba0c8dd169a2bea5bce43077d6
Suman Sarkar <69583200+sumanCEH@users.noreply.github.com>
2022-03-30 18:29:07 +0530

    Add files via upload
    testing

---
+++ merit/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('#signin_btn').click(function() {
+        var bt_box_object = bootbox.dialog({
+            title: 'Please Sign In',
+            closeButton: false,
+            message: $('#login_form').html(),
+            buttons: {
+                ok: {
+                    label: 'Sign In',
+                    callback: function() {
+                        bt_box_object.modal('hide')
+                    }
+                }
+            }
+        });
+    });
+});
